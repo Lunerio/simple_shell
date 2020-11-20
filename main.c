3203e30e58fdd185c52b3453a68bc01669ab2ba9
nicolasportela <2103@holbertonschool.com>
2020-11-20 16:08:09 +0000

    betty issues fixed

--- main.c
+++ main.c
@@ -9,33 +9,29 @@ int main(void)
 
 	for (i = 0; i < 64; i++)
 		argv[i] = malloc(64);
-	while(1)
+	while (1)
 	{
 		write(1, "($) ", 4);
 		string = get_newline(string);
 		if (string == NULL)
 		{
-			write(1, "\n", 1);
-			break;
+			write(1, "\n", 1), break;
 		}
 		if (string[0] == '\n')
 		{
-			write(1, "\n", 1);
-			continue;
+			write(1, "\n", 1), continue;
 		}
 		argv = tokenizer(string, argv);
 		child_pid = fork();
 		if (child_pid == -1)
 		{
-			perror("Error");
-			continue;
+			perror("Error"), continue;
 		}
 		if (child_pid == 0)
 		{
 			if (execve(argv[0], argv, NULL) == -1)
 			{
-				perror("Error");
-				continue;
+				perror("Error"), continue;
 			}
 		}
 		else
