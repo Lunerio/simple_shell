c86ec90a847a81fa9a59e6f641a578b69a89b5e1
Lunerio <lunerio@outlook.es>
2020-11-25 19:14:40 +0000

    trying to solve malloc

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 int main(int ac, char **av)
 {
 	char *string, **argv = NULL;
-	int glcheck, a, cp, lstring;
+	int glcheck = 0, a = 0, cp = 0;
 	size_t buf = 0;
 
 	(void)ac;
@@ -21,20 +21,20 @@ int main(int ac, char **av)
 		cp = p_prompt();
 		glcheck = getline(&string, &buf, stdin);
 
-		lstring = _strlen(string);
-		string[lstring - 1] = '\0';
-
 		a = checker(glcheck, string);
 		if (a == 2)
+		{
 			break;
+		}
 		if (a == 3)
+		{
 			write(1, "\n", 1);
+		}
 		if (a == 1)
 			perror(av[0]);
 		if (a == 4)
 		{
-			free(string);
-			continue;
+	        	continue;
 		}
 		if (a == 5)
 		{
